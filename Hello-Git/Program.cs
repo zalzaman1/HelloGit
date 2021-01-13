68a161bc76265b90f680453ee755a899bf295a35
zalzaman1 <zalzamani@my.dom.edu>
2021-01-12 20:31:02 -0500

    Initialize Repository

---
+++ Hello-Git/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGit
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GIT!");
+            Console.ReadLine();
+        }
+    }
+}
